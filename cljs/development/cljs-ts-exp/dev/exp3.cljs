d358cfd577a7590743bd29c815e7679a9bc137bf
Tiago Dall'Oca <tdalloca@gmail.com>
2021-08-18 07:44:50 -0300

    more tests + tuple + vector

--- cljs/development/cljs-ts-exp/dev/exp3.cljs
+++ cljs/development/cljs-ts-exp/dev/exp3.cljs
@@ -5,34 +5,69 @@
             [malli.registry :as mr]
             ["fs" :as fs]))
 
-(comment (mr/set-default-registry!
-          (mr/mutable-registry exp1/registry-db*)))
+(mr/set-default-registry!
+ (mr/mutable-registry exp1/registry-db*))
 
-(swap! exp1/registry-db* assoc :flow/person
-       [:map
-        [:name :string]
-        [:age :number]])
+(exp1/register-schema!
+ :flow/person
+ [:map
+  [:name :string]
+  [:age :number]])
+
+(exp1/register-schema!
+ :flow/family
+ [:vector :flow/person])
+
+(exp1/register-schema!
+ :flow/company
+ [:map
+  [:owners [:vector :flow/person]]
+  [:employees [:vector :flow/person]]
+  [:nested-type-test
+   [:map [:k1 :number] [:k2 :boolean]]]])
 
 (comment
   (mr/-schema @mr/registry* :flow/person))
 
 (def primitive-types-mapping
-  {:number {:str "number"}
-   :string {:str "string"}
-   :boolean {:str "boolean"}})
+  {:number {:literal "number"}
+   :literaling {:literal "string"}
+   :boolean {:literal "boolean"}})
 
 (def types-mapping
-  (merge
-   {:flow/person {:str (ts-export/->type-literal-str
-                        (mr/-schema @mr/registry* :flow/person)
-                        primitive-types-mapping)}}
-   primitive-types-mapping))
+  (loop [schema-ids [[:flow/person "FlowPerson"]
+                     [:flow/family "FlowFamily"]
+                     [:flow/company "FlowCompany"]]
+         types-mapping primitive-types-mapping]
+    (let [[s-id t-name] (first schema-ids)]
+      (if s-id
+        (recur
+         (rest schema-ids)
+         (merge types-mapping
+                {s-id {:type-name t-name
+                       :literal (ts-export/->type-literal-str
+                                 (mr/-schema @mr/registry* s-id)
+                                 types-mapping)}}))
+        types-mapping))))
+
+(comment (prn types-mapping))
 
 (comment
-  (let [file-content (ts-export/->type-declaration-str
-                      "FlowPerson"
-                      (-> types-mapping :flow/person :str)
-                      {:export? true})
+  (let [file-lines (map
+                    (fn [s-id]
+                      (let [{:keys [type-name str]} (-> types-mapping s-id)]
+                        (ts-export/->type-declaration-str
+                         type-name
+                         str
+                         {:export? true})))
+                    [:flow/person :flow/family :flow/company])
+        file-content (clojure.string/join "\n" file-lines)
         where "out/"]
     (fs/writeFile (str where "exp3.d.ts") file-content println)))
 
+
+(comment
+  (-> [:map [:a :number] [:b :string] [:c int?]]
+      m/children
+      (get-in [2 2])
+      m/type))
--- cljs/src/cljs_ts_exp/export/core.cljs
+++ cljs/src/cljs_ts_exp/export/core.cljs
@@ -2,9 +2,20 @@
   (:require [malli.core :as m]
             [camel-snake-kebab.core :as csk]))
 
+
 (defmulti ->type-literal-str
   (fn [schema _]
     (first schema)))
+
+(defn -get-type-literal
+  [typedef-registry s-form]
+  (let [another-schema? (vector? s-form)
+        type-literal (if another-schema?
+                       (->type-literal-str s-form typedef-registry)
+                       (or (-get-type-literal typedef-registry s-form)
+                           "any"))
+        typedef (get typedef-registry schema-id)]
+    (or (:type-name typedef) (:literal typedef))))
 
 (defmethod ->type-literal-str :map
   [schema typedef-registry]
@@ -12,14 +23,44 @@
         (for [[k opts child] (m/children schema)
               :let [entry-name (csk/->camelCaseString k)
                     s-form (m/-form child)
-                    another-schema? (coll? s-form)
-                    type-literal (if another-schema?
-                                   (->type-literal-str s-form typedef-registry)
-                                   (or (get-in typedef-registry [s-form :str])
-                                       "any"))
+                    type-literal (-get-type-literal typedef-registry s-form)
                     optional? (:optional opts)]]
           (str entry-name (when optional? "?") ":" type-literal))]
     (str "{"(clojure.string/join "," entry-literals) "}")))
+
+(comment
+  (->type-literal-str   
+   [:map
+    [:a :number]
+    [:b :string]
+    [:c :boolean]]
+   {:number {:literal "number"}
+    :string {:literal "string"}
+    :boolean {:literal "boolean"}}))
+
+(defmethod ->type-literal-str :vector
+  [[_ s-form] typedef-registry]
+  (let [another-schema-literal? (vector? s-form)
+        type-literal (-get-type-literal typedef-registry s-form)]
+    (str type-literal "[]")))
+
+(comment
+  (->type-literal-str [:vector :number] {:number {:literal "number"}}))
+
+(defmethod ->type-literal-str :tuple
+  [[_ & children] typedef-registry]
+  (let [type-literals
+        (for [s-form children
+              :let [another-schema-literal? (vector? s-form)]]
+          (-get-type-literal typedef-registry s-form))]
+    (str "[" (clojure.string/join "," type-literals) "]")))
+
+(comment
+  (->type-literal-str   
+   [:tuple :number :string :boolean]
+   {:number {:literal "number"}
+    :string {:literal "string"}
+    :boolean {:literal "boolean"}}))
 
 (defn ->type-declaration-str
   [type-name type-literal opts]
